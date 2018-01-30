069e55f5e678a87fa2b935685077cd23446d0228
ninjaproger <ninjaproger@gmail.com>
2018-01-30 00:11:19 -0800

    Define ImageCache protocol

--- RedditTop/RedditTop/ImageCache/ImageCache.swift
+++ RedditTop/RedditTop/ImageCache/ImageCache.swift
@@ -1,13 +1,16 @@
 //
-//  BaseImageCache.swift
+//  ImageCache.swift
 //  RedditTop
 //
 //  Created by Alexander Kharevich on 1/29/18.
 //  Copyright © 2018 Alexander Kharevich. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-protocol BaseImageCache {
-    
+protocol ImageCache {
+    func image(for url: URL) ->UIImage?
+    func set(image: UIImage, for url: URL)
+    func flush()
+    subscript(url: URL) -> UIImage? { get set }
 }
