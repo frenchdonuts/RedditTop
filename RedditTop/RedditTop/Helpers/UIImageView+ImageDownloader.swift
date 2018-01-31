c39461c147c47c9bd40f5d6ba2815fc3f8368115
ninjaproger <ninjaproger@gmail.com>
2018-01-31 15:02:23 -0800

    Implement UIImageView helper

--- RedditTop/RedditTop/Helpers/UIImageView+ImageDownloader.swift
+++ RedditTop/RedditTop/Helpers/UIImageView+ImageDownloader.swift
@@ -8,43 +8,79 @@
 
 import UIKit
 
-private struct AssociatedKeys {
-    static var downloadingTask = "downloadingTask"
-}
+private var downloadingTaskHandle: UInt8 = 0
+private var activityIndicatorViewHandle: UInt8 = 0
 
 extension UIImageView {
     static let cache = BasicInMemoryImageCache()
 
+    private var activityIndicatorView: UIActivityIndicatorView? {
+        get {
+            guard let activityIndicator = objc_getAssociatedObject(self, &activityIndicatorViewHandle) as? UIActivityIndicatorView else {
+                return nil
+            }
+            return activityIndicator
+        }
+
+        set(value) {
+            objc_setAssociatedObject(self,&activityIndicatorViewHandle, value,objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+
     private var downloadingTask: URLSessionDownloadTask? {
         get {
-            guard let task = objc_getAssociatedObject(self, &AssociatedKeys.downloadingTask) as? URLSessionDownloadTask else {
+            guard let task = objc_getAssociatedObject(self, &downloadingTaskHandle) as? URLSessionDownloadTask else {
                 return nil
             }
             return task
         }
 
         set(value) {
-            objc_setAssociatedObject(self,&AssociatedKeys.downloadingTask, value,objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            objc_setAssociatedObject(self,&downloadingTaskHandle, value,objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
 
-    func setImage(with url: URL, placeHolder: UIImage?) {
+    private func setupActivityIndicatorIfNeeded() {
+        guard self.activityIndicatorView == nil else { return }
+        let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        activityIndicatorView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(activityIndicatorView)
+        activityIndicatorView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        activityIndicatorView.stopAnimating()
+        activityIndicatorView.hidesWhenStopped = true
+        self.activityIndicatorView = activityIndicatorView
+    }
+
+    func setImage(with url: URL, placeHolder: UIImage?, completion: ((UIImage?)->Void)? = nil) {
         cancelDownload()
-        image = placeHolder
         if let cachedImage = UIImageView.cache[url] {
             image = cachedImage
+            completion?(cachedImage)
+            return
         }
-        downloadingTask = ImageDownloader.downloadImage(with: url) { [weak self] (downloadedImage, error) in
-            guard error == nil else { return }
-            guard let downloadedImage = downloadedImage else { return }
-            UIImageView.cache[url] = downloadedImage
-            self?.image = downloadedImage
-            self?.downloadingTask = nil
+        setupActivityIndicatorIfNeeded()
+        backgroundColor = UIColor.black
+        activityIndicatorView?.startAnimating()
+        downloadingTask = ImageDownloader.downloadImage(with: url) { [unowned self] (downloadedImage, error) in
+            DispatchQueue.main.async {
+                self.activityIndicatorView?.stopAnimating()
+                self.backgroundColor = UIColor.clear
+                guard error == nil, let downloadedImage = downloadedImage else {
+                    self.image = placeHolder
+                    self.downloadingTask = nil
+                    completion?(nil)
+                    return
+                }
+                UIImageView.cache[url] = downloadedImage
+                self.image = downloadedImage
+                self.downloadingTask = nil
+                completion?(downloadedImage)
+            }
         }
     }
 
     func cancelDownload() {
-        guard downloadingTask != nil else { return }
         downloadingTask?.cancel()
         downloadingTask = nil
     }
