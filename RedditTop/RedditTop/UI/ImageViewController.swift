b098a2c9eafca84ce88c2eadf835185d75cc3c19
ninjaproger <ninjaproger@gmail.com>
2018-01-31 15:03:41 -0800

    Implement ImageViewController

--- RedditTop/RedditTop/UI/ImageViewController.swift
+++ RedditTop/RedditTop/UI/ImageViewController.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2018 Alexander Kharevich. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import AssetsLibrary
+
+class ImageViewController: UIViewController {
+    var imageUrl: URL?
+    @IBOutlet weak var downloadButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBAction func onCloseButtonAction(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func onDownloadButtonAction(_ sender: Any) {
+        guard let image = imageView.image else { return }
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        downloadButton.isHidden = true
+        loadImage()
+    }
+
+    private func loadImage() {
+        guard let imageUrl = self.imageUrl else { return }
+        imageView.setImage(with: imageUrl, placeHolder: nil) { [unowned self] (image) in
+            self.downloadButton.isHidden = false
+        }
+    }
+}
