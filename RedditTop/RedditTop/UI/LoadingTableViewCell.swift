8e47abbd7faa8b6ea9c8439c956bdc730848e538
ninjaproger <ninjaproger@gmail.com>
2018-01-31 13:35:25 -0800

    Implement LoadingTableViewCell

--- RedditTop/RedditTop/UI/LoadingTableViewCell.swift
+++ RedditTop/RedditTop/UI/LoadingTableViewCell.swift
@@ -1,9 +1,13 @@
 //
-//  LoadingCell.swift
+//  LoadingTableViewCell.swift
 //  RedditTop
 //
 //  Created by Alexander Kharevich on 1/30/18.
 //  Copyright © 2018 Alexander Kharevich. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LoadingTableViewCell: UITableViewCell {
+    
+}
