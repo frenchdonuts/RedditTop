ca1ea8d18f517993185cead4f3fb5a6d4e04627d
ninjaproger <ninjaproger@gmail.com>
2018-01-31 13:33:10 -0800

    Add new files to the project

---
+++ RedditTop/RedditTop/Helpers/Data+TimeAgoString.swift
@@ -0,0 +1,9 @@
+//
+//  Data+TimeAgoString.swift
+//  RedditTop
+//
+//  Created by Alexander Kharevich on 1/30/18.
+//  Copyright © 2018 Alexander Kharevich. All rights reserved.
+//
+
+import Foundation
